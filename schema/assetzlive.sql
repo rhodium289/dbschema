63a1603cdfc92112fd396f7c3c9158edeefb0b61
Henry Grech-Cini <henry.grechcini@gmail.com>
2017-02-01 14:39:35 +0000

    Committing assetzlive.sql

---
+++ schema/assetzlive.sql
@@ -0,0 +1,41 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.5.2
+-- Dumped by pg_dump version 9.5.5
+
+-- Started on 2017-02-01 14:05:17 GMT
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET search_path = crm, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- TOC entry 665 (class 1259 OID 14074182)
+-- Name: acp-33-test-table; Type: TABLE; Schema: crm; Owner: assetz
+--
+
+CREATE TABLE "acp-33-test-table" (
+    id integer
+);
+
+
+ALTER TABLE "acp-33-test-table" OWNER TO assetz;
+
+-- Completed on 2017-02-01 14:05:19 GMT
+
+--
+-- PostgreSQL database dump complete
+--
+
